0f03169e273a9a2d58b610f7280061aba492d67d
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-01-09 18:42:33 +0800

    Added RecursiveInsertionSort

--- Sorting/insertionSort.cpp
+++ Sorting/insertionSort.cpp
@@ -14,6 +14,24 @@ void insertionSort(int *arr, const int size){
     }
 }
 
+void recursiveInsertionSort(int *arr, const int size, int tempI = 1){ // can be more optimized
+    if(tempI == size){
+        return;
+    }
+    for(int i = tempI; i >= 0; --i){
+        if(arr[i-1] > arr[i]){
+            std::swap(arr[i-1], arr[i]);
+        }
+    }
+    recursiveInsertionSort(arr, size, ++tempI);
+}
+
 int main(){
+    int arr[] = {10,9,8,7,6,5,4,3,2,1,0};
+    recursiveInsertionSort(arr,11);
+    for(auto i: arr){
+        std::cout << i << ' ';
+    }
+
     return 0;
 }
