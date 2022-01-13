537c12f6b59dd68b975b2c1f674eb71db8ec4962
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-01-13 12:19:13 +0800

    Added string deliminator tokenizer

--- Others/Tokenizer.cpp
+++ Others/Tokenizer.cpp
@@ -2,6 +2,7 @@
 #include <sstream>
 #include <vector>
 
+
 std::vector<std::string> tokenizeString(std::string str, char delim){ // using sstream
     std::stringstream ss(str);
     std::string indivStr;
@@ -12,8 +13,26 @@ std::vector<std::string> tokenizeString(std::string str, char delim){ // using s
     return strData;
 }
 
+std::vector<std::string> tokenizeString(std::string str, std::string delim){ // 
+    std::string indivStr;
+    std::vector<std::string> strData;
+    for(int i = 0; i < str.length(); ++i){
+        auto index = str.find(delim,i);
+        if(index != std::string::npos){
+            std::string subStr = str.substr(i,index-i);
+            strData.push_back(subStr);
+            i+=(delim.length()+subStr.size())-1;
+        }else{
+            strData.push_back(str.substr(i));
+            break;
+        }
+    }
+    return strData;
+}
+
+
 int main(){
-    for(auto i: tokenizeString("water;melon;papaya;banana water;melon;papaya;banana", ';')){
+    for(auto i: tokenizeString("papayawatermelonpapayawatermelonpapaya", "watermelon")){
         std::cout << i << '-';
     }
     std::cout << '\n';
